d60bb17958b57a53f01b82a9f5ef926dea68d071
Talha <mt452395@gmail.com>
2023-10-08 23:20:56 +0500

    doing some tasks

--- assignment1/index.js
+++ assignment1/index.js
@@ -1,2 +1,11 @@
 const fs = require('fs');
 const url = require('url')
+const express = require('express')
+// q1 create app
+const app =express()
+
+// q2 
+fs.open('doc.txt','w',(err,file)=>{
+    console.log(err,file);
+})
+fs.writeFile('.doc.txt','Product List',(err)=>console.log(err));
